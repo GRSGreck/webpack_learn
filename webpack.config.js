b2f7379633ac4842ea0fbf787bcfa71d4dfbad0d
roman <grechukromansergeevich@gmail.com>
2016-12-04 23:17:20 +0200

    styles component arh (loaders) - DOING

--- webpack.config.js
+++ webpack.config.js
@@ -4,27 +4,45 @@ module.exports = {
     context: `${ __dirname }/frontend`,
 
     entry: {
-        app: './app'
+        main: './main'
     },
 
     output: {
-        path: `${ __dirname }/public/js`,
+        path: `${ __dirname }/public`,
+        publicPath: '/',
         filename: '[name].js'
     },
+
+    watch: true,
+
     module: {
         loaders: [
             {
-                test: /old\.js$/,
-                // loader: 'expose?Work!imports?workSettings=>{delay:500}!exports?Work'
-                loader: 'script'
+                test: /\.js$/,
+                include: __dirname + '/frontend',
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.pug$/,
+                include: __dirname + '/frontend',
+                loader: 'pug-loader'
+            },
+            {
+                test: /\.css$/,
+                include: __dirname + '/frontend',
+                loaders: [
+                    'style',
+                    'css',
+                    'autoprefixer-loader?browsers=last 10 version'
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+                loader: 'file?name=[path][name].[ext]'
             }
         ]
-    },
-
-    resolve: {
-        root: __dirname + '/vendor',
-        alias: {
-            old: 'old/dist/old'
-        }
     }
 };
